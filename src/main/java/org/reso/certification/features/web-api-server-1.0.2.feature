0483881a91043dbeab91fb6f773b9a52885f36b2
Joshua Darnell <josh@kurotek.com>
2020-02-12 01:01:33 -0800

    upgraded dependencies and got tests through DataSystem working

--- src/main/java/org/reso/certification/features/web-api-server-1.0.2.feature
+++ src/main/java/org/reso/certification/features/web-api-server-1.0.2.feature
@@ -9,7 +9,7 @@ Feature: Web API Server 1.0.2 Certification
 
   @REQ-WA103-END3 @core
   Scenario: REQ-WA103-QR3 - CORE - Request and Validate Server Metadata
-    When a /$metadata request is issued relative to the ClientSettings_WebAPIURI
+    When a metadata request is made relative to the given service root in ClientSettings_WebAPIURI
     Then the server responds with a status code of 200
     And the metadata returned is valid
 
@@ -18,12 +18,13 @@ Feature: Web API Server 1.0.2 Certification
     Given a case-sensitive OData $select list consisting of Parameter_SelectList in the given request
     When the request is issued against the Web API Server's Parameter_EndpointResource
     Then the server responds with a status code of 200
-    And the results contain at most Parameter_TopCount records with data present in the fields in Parameter_SelectList
+    And the results contain at most Parameter_TopCount records
+    And data are present in fields contained within Parameter_SelectList
 
   @REQ_WA103_END2 @core
   Scenario: REQ-WA103-END2 - CORE - Data Systems Endpoint test
-    Given the url for the server's DataSystems endpoint
-    When the request is issued against the Web API Server's Parameter_EndpointResource
+    Given the url for the server's DataSystem endpoint
+    When the request is issued against the DataSystem endpoint
     Then the server responds with a status code of 200
     And the results are valid JSON
-    And the results match the expected DataSystems schema
+    And the results match the expected DataSystem schema
--- src/main/java/org/reso/certification/stepdefs/WebAPIServer_1_0_2.java
+++ src/main/java/org/reso/certification/stepdefs/WebAPIServer_1_0_2.java
@@ -1,7 +1,9 @@
 package org.reso.certification.stepdefs;
 
+import com.github.fge.jackson.JacksonUtils;
 import io.cucumber.java8.En;
 
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
@@ -15,8 +17,14 @@ import org.reso.models.Request;
 import org.reso.models.Settings;
 
 import java.io.*;
+import java.net.URI;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static io.restassured.path.json.JsonPath.from;
 import static org.junit.Assert.*;
 
 public class WebAPIServer_1_0_2 implements En {
@@ -38,13 +46,13 @@ public class WebAPIServer_1_0_2 implements En {
       if (pathToRESOScript == null) {
         pathToRESOScript = System.getProperty("pathToRESOScript");
       }
-      LOG.debug("Using RESOScript: " + pathToRESOScript);
+      LOG.info("Using RESOScript: " + pathToRESOScript);
     });
     And("^Client Settings and Parameters were read from the file$", () -> {
       if (settings == null) {
         settings = Settings.loadFromRESOScript(new File(System.getProperty("pathToRESOScript")));
       }
-      LOG.debug("RESOScript loaded successfully!");
+      LOG.info("RESOScript loaded successfully!");
     });
 
     Given("^an OData client was successfully created from the given RESOScript$", () -> {
@@ -53,7 +61,7 @@ public class WebAPIServer_1_0_2 implements En {
       //TODO: add base64 un-encode when applicable
       bearerToken = settings.getClientSettings().get(ClientSettings.BEARER_TOKEN);
       if (bearerToken != null && bearerToken.length() > 0) {
-        LOG.debug("Bearer token loaded... first 4 characters: " + bearerToken.substring(0, 4));
+        LOG.info("Bearer token loaded... first 4 characters: " + bearerToken.substring(0, 4));
       }
 
       clientId = settings.getClientSettings().get(ClientSettings.CLIENT_IDENTIFICATION);
@@ -63,7 +71,7 @@ public class WebAPIServer_1_0_2 implements En {
       redirectUri = settings.getClientSettings().get(ClientSettings.REDIRECT_URI);
       scope = settings.getClientSettings().get(ClientSettings.CLIENT_SCOPE);
 
-      LOG.debug("Service root is: " + serviceRoot);
+      LOG.info("Service root is: " + serviceRoot);
 
       //create Commander instance
       commander.set(new Commander.Builder()
@@ -83,43 +91,96 @@ public class WebAPIServer_1_0_2 implements En {
     AtomicReference<ODataRawResponse> oDataRawResponse = new AtomicReference<>();
     AtomicReference<Request> request = new AtomicReference<>();
     AtomicReference<ODataRawRequest> rawRequest = new AtomicReference<>();
+    AtomicReference<String> jsonResponseData = new AtomicReference<>();
+    AtomicReference<Integer> numResults = new AtomicReference<>();
 
     //REQ-WA103-END3
-    When("^a /\\$metadata request is issued relative to the ClientSettings_WebAPIURI$", () -> {
+    When("^a metadata request is made relative to the given service root in ClientSettings_WebAPIURI$", () -> {
       request.set(Settings.resolveParameters(settings.getRequests().get(REQUESTS.WEB_API_1_0_2.REQ_WA_103_END_3), settings));
 
-      LOG.debug("Request URL: " + request.get().getUrl());
+      LOG.info("Request URL: " + request.get().getUrl());
 
       rawRequest.set(commander.get().getClient().getRetrieveRequestFactory().getRawRequest(Commander.prepareURI(request.get().getUrl())));
       oDataRawResponse.set(rawRequest.get().execute());
     });
-
     Then("^the server responds with a status code of (\\d+)$", (Integer code) -> {
-      assertEquals(code.intValue(), oDataRawResponse.get().getStatusCode());
+      int responseCode = oDataRawResponse.get().getStatusCode();
+      LOG.info("Response code is: " + responseCode);
+      assertEquals(code.intValue(), responseCode);
     });
     And("^the metadata returned is valid$", () -> {
-      assertTrue(commander.get().validateMetadata((oDataRawResponse.get().getRawResponse())));
+      boolean isValid = commander.get().validateMetadata((oDataRawResponse.get().getRawResponse()));
+      LOG.info("Metadata is: " + (isValid ? "valid" : "invalid"));
+      assertTrue(isValid);
     });
+
 
     //REQ-WA103-QR3
     Given("^a case-sensitive OData \\$select list consisting of Parameter_SelectList in the given request$", () -> {
       request.set(Settings.resolveParameters(settings.getRequests().get(REQUESTS.WEB_API_1_0_2.REQ_WA_103_QR_3), settings));
-      LOG.debug("Request URL: " + request.get().getUrl());
+      LOG.info("Request URL: " + request.get().getUrl());
+      assertTrue(request.get().getUrl().contains("$select=" + settings.getParameters().getValue("SelectList")));
     });
     When("^the request is issued against the Web API Server's Parameter_EndpointResource$", () -> {
-      rawRequest.set(commander.get().getClient().getRetrieveRequestFactory().getRawRequest(Commander.prepareURI(request.get().getUrl())));
+      URI requestUri = Commander.prepareURI(request.get().getUrl());
+
+      LOG.info("Request is: " + requestUri.toString());
+
+      rawRequest.set(commander.get().getClient().getRetrieveRequestFactory().getRawRequest(requestUri));
       oDataRawResponse.set(rawRequest.get().execute());
     });
-    /* NOTE: response code testing is done with the Then above ^^ */
-    And("^the results contain at most Parameter_TopCount records with data present in the fields in Parameter_SelectList$", () -> {
-      String jsonString = convertInputStreamToString(oDataRawResponse.get().getRawResponse());
-      LOG.debug("Server response is: " + jsonString);
+    And("^the results contain at most Parameter_TopCount records$", () -> {
+      jsonResponseData.set(convertInputStreamToString(oDataRawResponse.get().getRawResponse()));
+      LOG.info("Server response is: " + jsonResponseData.get());
+
+      //TODO: create constants for parameter or take as args to this function
+      List<String> items = from(jsonResponseData.get()).getList("value");
+      numResults.set(items.size());
+
+      LOG.info("Number of values returned: " + numResults.get());
+      assertTrue(numResults.get() <= Integer.parseInt(settings.getParameters().getValue("TopCount")));
     });
-    Given("^the url for the server's DataSystems endpoint$", () -> {
+    And("^data are present in fields contained within Parameter_SelectList$", () -> {
+      List<String> fieldList = new ArrayList<>();
+      Arrays.stream(settings.getParameters().getValue("SelectList").split(",")).forEach(field -> fieldList.add(field));
+
+      AtomicInteger numFieldsWithData = new AtomicInteger();
+
+      from(jsonResponseData.get()).getList("value", HashMap.class).forEach(item -> {
+        if (item != null) {
+          fieldList.forEach(field -> {
+            if (item.get(field) != null) {
+              numFieldsWithData.getAndIncrement();
+            }
+          });
+        }
+      });
+
+      LOG.info("numResults: " + numResults.get());
+      LOG.info("numFields: " + fieldList.size());
+      LOG.info("numFieldsWithData: " + numFieldsWithData.get());
+      LOG.info("percent field fill: " + ((numResults.get() * fieldList.size()) / (1.0 * numFieldsWithData.get()) * 100) + "%");
+      assertTrue(numFieldsWithData.get() > 0);
+    });
+
+
+    //REQ-WA103-END2
+    Given("^the url for the server's DataSystem endpoint$", () -> {
+      request.set(Settings.resolveParameters(settings.getRequests().get(REQUESTS.WEB_API_1_0_2.REQ_WA103_END2), settings));
+      String dataSystemUrlString = request.get().getUrl();
+      LOG.info("Data System URL: " + dataSystemUrlString);
+      assertTrue(dataSystemUrlString.length() > 0);
+    });
+    When("^the request is issued against the DataSystem endpoint$", () -> {
+      rawRequest.set(commander.get().getClient().getRetrieveRequestFactory().getRawRequest(Commander.prepareURI(request.get().getUrl())));
+      oDataRawResponse.set(rawRequest.get().execute());
+      jsonResponseData.set(convertInputStreamToString(oDataRawResponse.get().getRawResponse()));
+      LOG.info("Response is: " + jsonResponseData.get());
     });
     And("^the results are valid JSON$", () -> {
+      assertTrue(JacksonUtils.getReader().readTree(jsonResponseData.get()).size() > 0);
     });
-    And("^the results match the expected DataSystems schema$", () -> {
+    And("^the results match the expected DataSystem schema$", () -> {
     });
   }
 
@@ -127,6 +188,7 @@ public class WebAPIServer_1_0_2 implements En {
     private static class WEB_API_1_0_2 {
       private static final String REQ_WA_103_END_3 = "REQ-WA103-END3";
       private static final String REQ_WA_103_QR_3 = "REQ-WA103-QR3";
+      private static final String REQ_WA103_END2 = "REQ-WA103-END2";
     }
   }
 
